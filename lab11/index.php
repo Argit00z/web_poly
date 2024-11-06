f21dbe3d382f440c1f8de3db5fa5a966fec73ec6
Arman Galstyan <galstyanarman05@gmail.com>
2024-11-06 12:14:05 +0300

    lab11,12

--- lab11/index.php
+++ lab11/index.php
@@ -7,17 +7,41 @@
         body {
             font-family: Arial, sans-serif;
         }
-        #main_menu, #product_menu {
+        #main_menu {
             margin: 20px;
+            display: flex;
         }
-        #main_menu a, #product_menu a {
+        #main_menu a {
             margin-right: 10px;
             padding: 5px 10px;
             text-decoration: none;
             border: 1px solid #ccc;
         }
+        #product_menu {
+            margin: 20px;
+            display: flex;
+            flex-direction: column;
+            width: 200px;
+        }
+        #product_menu a {
+            margin: 5px 0;
+            padding: 10px 20px;
+            text-decoration: none;
+            border: 1px solid #ccc;
+        }
         .selected {
             background-color: #ddd;
+        }
+        table {
+            border-collapse: collapse;
+            margin: 10px 0;
+        }
+        table, th, td {
+            border: 1px solid #000;
+        }
+        th, td {
+            padding: 10px;
+            text-align: center;
         }
         .ttRow, .ttSingleRow {
             display: flex;
@@ -33,6 +57,13 @@
         }
         .ttCell a {
             text-decoration: none;
+        }
+        #content_wrapper {
+            display: flex;
+        }
+        #main_content {
+            flex-grow: 1;
+            margin-left: 20px;
         }
     </style>
 </head>
@@ -52,62 +83,87 @@
     ?>
 </div>
 
-<div id="product_menu">
-    <?php
-    echo '<a href="/' . ($html_type ? '?html_type=' . $html_type : '') . '"';
-    if (!$content) echo ' class="selected"';
-    echo '>Вся таблица умножения</a>';
+<div id="content_wrapper">
+    <div id="product_menu">
+        <?php
+        echo '<a href="/' . ($html_type ? '?html_type=' . $html_type : '') . '"';
+        if (!$content) echo ' class="selected"';
+        echo '>Вся таблица умножения</a>';
 
-    for ($i = 2; $i <= 9; $i++) {
-        echo '<a href="?content=' . $i . ($html_type ? '&html_type=' . $html_type : '') . '"';
-        if ($content == $i) echo ' class="selected"';
-        echo '>Таблица умножения на ' . $i . '</a>';
-    }
-    ?>
-</div>
+        for ($i = 2; $i <= 9; $i++) {
+            echo '<a href="?content=' . $i . ($html_type ? '&html_type=' . $html_type : '') . '"';
+            if ($content == $i) echo ' class="selected"';
+            echo '>Таблица умножения на ' . $i . '</a>';
+        }
+        ?>
+    </div>
 
-<div id="main_content">
-    <?php
-    function generateTable($number = null, $isTable = true)
-    {
-        $output = '';
+    <div id="main_content">
+        <?php
+        function generateTable($number = null, $isTable = true)
+        {
+            $output = $isTable ? '<table>' : '';
 
-        if ($number === null) {
-            for ($i = 2; $i <= 9; $i++) {
-                $output .= generateRow($i, $isTable);
+            if ($number === null) {
+                for ($i = 2; $i <= 9; $i++) {
+                    $output .= generateRow($i, $isTable);
+                }
+            } else {
+                $output .= generateRow($number, $isTable);
             }
+
+            $output .= $isTable ? '</table>' : '';
+
+            return $output;
+        }
+
+        function generateRow($number, $isTable)
+        {
+            $output = $isTable ? '<tr>' : '<div class="ttSingleRow">';
+
+            for ($i = 1; $i <= 9; $i++) {
+                if ($isTable) {
+                    $output .= '<td>';
+                } else {
+                    $output .= '<div class="ttCell">';
+                }
+                if ($number <= 9) {
+                    $output .= '<a href="?content=' . $number . '">' . $number . '</a>';
+                } else {
+                    $output .= $number;
+                }
+                $output .= ' x ';
+                if ($i <= 9) {
+                    $output .= '<a href="?content=' . $i . '">' . $i . '</a>';
+                } else {
+                    $output .= $i;
+                }
+                $output .= ' = ';
+                $result = $number * $i;
+                if ($result <= 9) {
+                    $output .= '<a href="?content=' . $result . '">' . $result . '</a>';
+                } else {
+                    $output .= $result;
+                }
+                if ($isTable) {
+                    $output .= '</td>';
+                } else {
+                    $output .= '</div>';
+                }
+            }
+
+            $output .= $isTable ? '</tr>' : '</div>';
+
+            return $output;
+        }
+
+        if ($html_type == 'TABLE') {
+            echo generateTable($content ? (int)$content : null, true);
         } else {
-            $output .= generateRow($number, $isTable);
+            echo generateTable($content ? (int)$content : null, false);
         }
-
-        return $output;
-    }
-
-    function generateRow($number, $isTable)
-    {
-        $output = $isTable ? '<div class="ttRow">' : '<div class="ttSingleRow">';
-
-        for ($i = 1; $i <= 9; $i++) {
-            $output .= '<div class="ttCell">';
-            $output .= '<a href="?content=' . $number . '">' . $number . '</a>';
-            $output .= ' x ';
-            $output .= '<a href="?content=' . $i . '">' . $i . '</a>';
-            $output .= ' = ';
-            $output .= '<a href="?content=' . ($number * $i) . '">' . ($number * $i) . '</a>';
-            $output .= '</div>';
-        }
-
-        $output .= '</div>';
-
-        return $output;
-    }
-
-    if ($html_type == 'TABLE') {
-        echo generateTable($content ? (int)$content : null, true);
-    } else {
-        echo generateTable($content ? (int)$content : null, false);
-    }
-    ?>
+        ?>
+    </div>
 </div>
 
 <div id="footer">
